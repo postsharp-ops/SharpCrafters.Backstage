a62dfdd385b1a99f8f06e0548f06b17709f789a7
Gael Fraiteur <gael@postsharp.net>
2025-07-02 18:59:59 +0200

    Warning.

--- src/Metalama.Backstage/UserInterface/WindowsUserInterfaceService.cs
+++ src/Metalama.Backstage/UserInterface/WindowsUserInterfaceService.cs
@@ -2,12 +2,12 @@
 // SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
 // Refer to LICENSE.md in the repository root for complete details.
 
-using Metalama.Backstage.Diagnostics;
 using Metalama.Backstage.Extensibility;
 using Metalama.Backstage.Tools;
 using System;
 
 #if NETFRAMEWORK || NETCOREAPP
+using Metalama.Backstage.Diagnostics;
 using Microsoft.Win32;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
