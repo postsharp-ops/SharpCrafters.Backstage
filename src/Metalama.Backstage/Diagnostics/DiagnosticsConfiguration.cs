4179c06a804767787c9b31ddaf81071f4ea7145c
teamcity <teamcity@postsharp.net>
2026-08-01 02:46:29 -0700

    Read the debugging, crashDumps and profiling sections of diagnostics.json (#1778)

--- src/Metalama.Backstage/Diagnostics/DiagnosticsConfiguration.cs
+++ src/Metalama.Backstage/Diagnostics/DiagnosticsConfiguration.cs
@@ -18,36 +18,98 @@ public sealed record DiagnosticsConfiguration : ConfigurationFile
 {
     public const string EnvironmentVariableName = "METALAMA_DIAGNOSTICS";
 
+    /// <summary>
+    /// Gets the options of the logging of Metalama itself.
+    /// </summary>
     [JsonPropertyName( "logging" )]
-    public LoggingConfiguration Logging { get; init; } = new();
+    public LoggingConfiguration Logging { get; init; } = CreateDefaultLogging();
 
+    /// <summary>
+    /// Gets the options that make a Metalama process wait for a debugger to be attached.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The property needs an <c>init</c> accessor. System.Text.Json serializes a get-only property, but it cannot
+    /// deserialize into it, and <see cref="DebuggerConfiguration"/> is immutable, so it cannot be populated in place
+    /// either. The section was therefore read from the file and then discarded. See #1778.
+    /// </para>
+    /// <para>
+    /// The null value has to be normalized because the System.Text.Json source generator treats every <c>init</c>
+    /// property as a constructor parameter and assigns all of them unconditionally, so a file that omits the section
+    /// would otherwise set the property to <c>null</c>.
+    /// </para>
+    /// </remarks>
     [JsonPropertyName( "debugging" )]
-    public DebuggerConfiguration Debugging { get; } = new();
-
-    [JsonPropertyName( "crashDumps" )]
-    public CrashDumpConfiguration CrashDumps { get; } = new();
-
-    [JsonPropertyName( "profiling" )]
-    public ProfilingConfiguration Profiling { get; } = new();
-
-    public DiagnosticsConfiguration()
+    public DebuggerConfiguration Debugging
     {
-        var processes = Enum.GetValues( typeof(ProcessKind) )
-            .Cast<ProcessKind>()
-            .ToImmutableDictionary( x => x.ToString(), _ => false );
+        get => this._debugging;
+        init => this._debugging = value ?? CreateDefaultDebugging();
+    }
 
-        this.Logging = new LoggingConfiguration
+    /// <summary>
+    /// Gets the options of the collection of crash dumps.
+    /// </summary>
+    /// <remarks>
+    /// See the remarks of <see cref="Debugging"/> for the reason why this property has an <c>init</c> accessor and
+    /// normalizes the null value.
+    /// </remarks>
+    [JsonPropertyName( "crashDumps" )]
+    public CrashDumpConfiguration CrashDumps
+    {
+        get => this._crashDumps;
+        init => this._crashDumps = value ?? CreateDefaultCrashDumps();
+    }
+
+    /// <summary>
+    /// Gets the options of the profiling of Metalama processes.
+    /// </summary>
+    /// <remarks>
+    /// See the remarks of <see cref="Debugging"/> for the reason why this property has an <c>init</c> accessor and
+    /// normalizes the null value.
+    /// </remarks>
+    [JsonPropertyName( "profiling" )]
+    public ProfilingConfiguration Profiling
+    {
+        get => this._profiling;
+        init => this._profiling = value ?? CreateDefaultProfiling();
+    }
+
+    private readonly DebuggerConfiguration _debugging = CreateDefaultDebugging();
+    private readonly CrashDumpConfiguration _crashDumps = CreateDefaultCrashDumps();
+    private readonly ProfilingConfiguration _profiling = CreateDefaultProfiling();
+
+    /// <summary>
+    /// The default value of the <c>processes</c> member of every section: all known kinds of processes, disabled.
+    /// </summary>
+    private static readonly ImmutableDictionary<string, bool> _defaultProcesses = Enum.GetValues( typeof(ProcessKind) )
+        .Cast<ProcessKind>()
+        .ToImmutableDictionary( x => x.ToString(), _ => false );
+
+    /// <summary>
+    /// Creates the default value of the <see cref="Logging"/> property.
+    /// </summary>
+    private static LoggingConfiguration CreateDefaultLogging()
+        => new()
         {
-            Processes = processes,
+            Processes = _defaultProcesses,
             TraceCategories = ImmutableDictionary<string, bool>.Empty.WithComparers( StringComparer.OrdinalIgnoreCase ).Add( "*", false )
         };
 
-        this.Debugging = new DebuggerConfiguration() { Processes = processes };
+    /// <summary>
+    /// Creates the default value of the <see cref="Debugging"/> property.
+    /// </summary>
+    private static DebuggerConfiguration CreateDefaultDebugging() => new() { Processes = _defaultProcesses };
 
-        this.Profiling = new ProfilingConfiguration() { Kind = "performance", Processes = processes };
+    /// <summary>
+    /// Creates the default value of the <see cref="CrashDumps"/> property.
+    /// </summary>
+    private static CrashDumpConfiguration CreateDefaultCrashDumps()
+        => new() { Processes = _defaultProcesses, ExceptionTypes = ImmutableArray.Create( "*" ) };
 
-        this.CrashDumps = new CrashDumpConfiguration() { Processes = processes, ExceptionTypes = ImmutableArray.Create( "*" ) };
-    }
+    /// <summary>
+    /// Creates the default value of the <see cref="Profiling"/> property.
+    /// </summary>
+    private static ProfilingConfiguration CreateDefaultProfiling() => new() { Kind = "performance", Processes = _defaultProcesses };
 
     public override void Validate( Action<string> reportWarning )
     {
